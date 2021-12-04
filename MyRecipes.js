e416ef859bdb89d5ba15f94872028734d6ad62db
darshil788 <83503144+darshil788@users.noreply.github.com>
2021-12-04 15:36:59 +0400

    Add files via upload

---
+++ MyRecipes.js
@@ -0,0 +1,14 @@
+MyRecipes
+README.md
+ingredients.md
+cucumber
+carrot
+tomato
+bread
+recipe.md
+cut cucumber
+cut carrot
+cut tomato
+put cucumber on bread
+put carrot on bread
+put tomato on bread
